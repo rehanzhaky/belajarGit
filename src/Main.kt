9e80ddd57d795f90f34eadae990945b6cb2e1906
rehanzhaky <hannn9491@gmail.com>
2025-03-14 09:55:02 +0700

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,16 @@
+// Function
+
+fun main() {
+    tadarus()
+   bukber()
+}
+
+fun bukber() {
+    var takjil = "Kurma"
+    println(takjil)
+}
+
+fun tadarus() {
+    var mengaji = "surah"
+    print(mengaji)
+}
